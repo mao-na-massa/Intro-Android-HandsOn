b97b42cfd1797225df74146f4e35e14230dce6d2
Guilherme Hayashi <guilhermehayashi@Guilhermes-MacBook-Pro-2.local>
2019-03-05 15:42:48 -0300

    Interação de adicionar uma nova atividade
    - Layout para adicionar uma nova atividade
    - Implementar a interação de adicionar uma nova atividade.

--- app/src/main/java/com/cheesecakelabs/mnm_todoistsample/TodoListActivity.kt
+++ app/src/main/java/com/cheesecakelabs/mnm_todoistsample/TodoListActivity.kt
@@ -1,13 +1,24 @@
 package com.cheesecakelabs.mnm_todoistsample
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_todo_list.*
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
+
 
 class TodoListActivity : AppCompatActivity() {
 
+    /*
+    *
+    * Variáveis e tipos de dados
+    *
+    * */
+
     var listOfTasks = mutableListOf<String>()
+    var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,13 +27,25 @@ class TodoListActivity : AppCompatActivity() {
         todoList.layoutManager = LinearLayoutManager(this)
         todoList.adapter = TodoListAdapter(this, listOfTasks as ArrayList<String>)
 
-        listOfTasks.add("TESTE")
-        listOfTasks.add("TESTE 1")
-        listOfTasks.add("TESTE 2")
-        listOfTasks.add("TESTE 3")
-        listOfTasks.add("TESTE 4")
-        listOfTasks.add("TESTE 5")
+        newTaskButton.setOnClickListener {
+            addNewTask()
+        }
 
+    }
 
+    fun addNewTask() {
+        var task = newTaskEdit.text.toString()
+        listOfTasks.add(task)
+        count = count + 1
+        todoList.adapter.notifyDataSetChanged()
+        todoList.scrollToPosition(listOfTasks.size - 1)
+        clearNewTask()
+    }
+
+    fun clearNewTask() {
+        newTaskEdit.setText("")
+        newTaskEdit.clearFocus()
+        val imm = newTaskEdit.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(newTaskEdit.getWindowToken(), 0)
     }
 }
